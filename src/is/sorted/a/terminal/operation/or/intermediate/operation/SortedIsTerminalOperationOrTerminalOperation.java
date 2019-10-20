0f7b0b76b9a62248f26ba0b418f927ba1ebcd13e
Faraz Durrani <farazkhurram@msn.com>
2019-10-19 23:42:52 -0500

    corrected a mistake where sorted function on stream was stated as terminal. It is not terminal.

--- src/is/sorted/a/terminal/operation/or/intermediate/operation/SortedIsTerminalOperationOrTerminalOperation.java
+++ src/is/sorted/a/terminal/operation/or/intermediate/operation/SortedIsTerminalOperationOrTerminalOperation.java
@@ -2,7 +2,7 @@ package is.sorted.a.terminal.operation.or.intermediate.operation;
 
 import java.util.Arrays;
 
-public class SortedIsTerminalOperation {
+public class SortedIsTerminalOperationOrTerminalOperation {
 	public static void main(String[] args) {
 		int arr[] = { 1, 2, 3, 4, 5 };
 
@@ -15,7 +15,7 @@ public class SortedIsTerminalOperation {
 				System.err.println("Map called on value ->" + n);
 				return n;
 			})
-			.sorted() //SORTED IS TERMINAL OPERATION
+			.sorted() //SORTED IS TERMINAL OPERATION. Terminal means, stream ended.
 			.map(n -> {
 				System.err.println("Map called twice on value ->" + n);
 				return n;
