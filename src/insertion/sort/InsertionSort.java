24f2fc79ab822568fac39c30f8b17546b6a88403
Faraz Durrani <farazkhurram@msn.com>
2020-07-18 20:27:26 -0500

    Multiple Insertion Sort practice

--- src/insertion/sort/InsertionSort.java
+++ src/insertion/sort/InsertionSort.java
@@ -6,18 +6,49 @@ import java.util.stream.IntStream;
 public class InsertionSort {
 	public static void main(String[] args) {
 		int[] arr = { 5, 3, 6, 4, 1, 2 };
-		Arrays.stream(arr).forEach(System.out::print);
-		for (int i = 1; i < arr.length; i++) {
-			int currKey = arr[i];
+		Arrays.stream(arr)
+			.forEach(System.out::print);
+		for (int i = 0; i < arr.length; i++) {
+			int key = arr[i];
 			int j = i - 1;
-			while (j > -1 && arr[j] > currKey) {
+			while(j > -1 && arr[j] > key) {
 				arr[j+1] = arr[j];
 				j--;
 			}
-			arr[j + 1] = currKey;
+			arr[j+1] = key;
 		}
 		System.out.println();
-		IntStream.of(arr).forEach(System.out::print);
+		IntStream.of(arr)
+			.forEach(System.out::print);
 
 	}
+//	public static void main(String[] args) {
+//		int[] arr = { 5, 3, 6, 4, 1, 2 };
+//		Arrays.stream(arr).forEach(System.out::print);
+//		for (int i = 1; i < arr.length; i++) {
+//			int currKey = arr[i];
+//			int j = i - 1;
+//			while (j > -1 && arr[j] > currKey) {
+//				arr[j+1] = arr[j];
+//				j--;
+//			}
+//			arr[j + 1] = currKey;
+//		}
+//		System.out.println();
+//		IntStream.of(arr).forEach(System.out::print);
+//
+//	}
+//	int[] arr = { 5, 3, 6, 4, 1, 2 };
+//	Arrays.stream(arr).forEach(System.out::print);
+//	for (int i = 1; i < arr.length; i++) {
+//		int key = arr[i];
+//		int j = i - 1;
+//		while (j > -1 && arr[j] > key) {
+//			arr[j+1] = arr[j];
+//			j--;
+//		}
+//		arr[j+1] = key;
+//	}
+//	System.out.println();
+//	IntStream.of(arr).forEach(System.out::print);
 }
